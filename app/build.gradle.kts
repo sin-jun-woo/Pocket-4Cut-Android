022fc33c4777889060e72c4664152c4ab3aa65a6
SHIN-JUN-WOO <46078573+sin-jun-woo@users.noreply.github.com>
2026-09-10 22:47:26 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.pocket4cut"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
